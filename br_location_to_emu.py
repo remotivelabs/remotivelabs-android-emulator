fbd11ebec064d3c8f4a05d50bdf119c6d551dd9b
Aleksandar Filipov <aleksandar@remotivelabs.com>
2023-11-09 12:43:56 +0100

    allow arbitry location signal names

--- br_location_to_emu.py
+++ br_location_to_emu.py
@@ -6,6 +6,9 @@ class brokerToEmu:
     def __init__(self, adb_dev):
         # Get emulator via adb only once
         self.adb_dev = adb_dev
+        self.args_url, self.args_api_key, self.signals = parsing_to_subscribe()
+        self.signal_name_latitude = self.signals[0][1]
+        self.signal_name_longitude = self.signals[1][1]
         self.lon = None
         self.lat = None
 
@@ -16,10 +19,9 @@ class brokerToEmu:
                     signal.id.name, signal.id.namespace.name, signal.double
                 )
             )
-
-            if signal.id.name == "LATITUDE":
+            if signal.id.name == self.signal_name_latitude:
                 self.lat = signal.double
-            elif signal.id.name == "LONGITUDE":
+            elif signal.id.name == self.signal_name_longitude:
                 self.lon = signal.double
 
             if self.lat is not None and self.lon is not None:
@@ -27,8 +29,7 @@ class brokerToEmu:
                 self.adb_dev.send_fix(str(self.lon), str(self.lat))
 
     def update_emu_location(self):
-        args_url, args_api_key, signals = parsing_to_subscribe()
-        subscribe(args_url, args_api_key, signals, on_subscribe=self.redirect_location_to_emulator, on_change=False)
+        subscribe(self.args_url, self.args_api_key, self.signals, on_subscribe=self.redirect_location_to_emulator, on_change=False)
 
 
 if __name__ == "__main__":
