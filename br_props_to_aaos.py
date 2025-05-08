6d3d8942739da0c329158d189cfedad8f1896dd0
Ola Nilsson <ola.nilsson@remotivelabs.com>
2025-05-08 11:27:21 +0200

    init!

--- br_props_to_aaos.py
+++ br_props_to_aaos.py
@@ -1,4 +1,5 @@
 import libs.adb.device as adb
+import libs.adb.device as adb
 import libs.vhal_emulator.vhal_emulator as vhal_emu
 from libs.vhal_emulator.vhal_prop_consts_2_0 import vhal_props, vhal_vehicle_area
 from libs.remotive.subscribe import parsing_to_subscribe, subscribe, get_proper_signal_value
@@ -10,13 +11,14 @@ class brokerDeviceBridge:
         # Get the connected device via adb only once
         self.adb_dev = adb_dev
         self.vhal = vhal_emu.Vhal()
+        print("Available VHAL properties:", self.vhal._propToType)
 
     def redirect_vhal_props_to_device(self, signals):
         for signal in signals:
             signal_val = get_proper_signal_value(signal)
             print(
-                "{} {} {} ".format(
-                    signal.id.name, signal.id.namespace.name, signal_val
+                "{} {} {} {}".format(
+                    signal.id.name, signal.id.namespace.name, signal_val, type(signal_val)
                 )
             )
             for prop in getmembers(vhal_props):
@@ -25,11 +27,20 @@ class brokerDeviceBridge:
                     # In the future it'd be nice to get such info without hard coding it.
                     # Refer to https://developer.android.com/reference/android/car/VehicleAreaType.
                     print("Setting property on AAOS...")
-                    self.vhal.set_property(signal.id.name, vhal_vehicle_area.GLOBAL, signal_val)
+                    property_id = 291504647  # The property ID you want to set
+                    area_id = vhal_vehicle_area.GLOBAL  # Try GLOBAL first; if no effect, try 0
+                    value = round(signal_val, 1)
+                    self.vhal.set_property(property_id, area_id, value)
+                    property_id = 291504648  # The property ID you want to set
+                    area_id = vhal_vehicle_area.GLOBAL  # Try GLOBAL first; if no effect, try 0
+                    value = round(signal_val, 1)
+                    self.vhal.set_property(property_id, area_id, value)
+
+                    #self.vhal.set_property(0x15600503, vhal_vehicle_area.GLOBAL, 1001.0)
 
     def set_vhal_props(self):
         args_url, args_api_key, _cvd, signals = parsing_to_subscribe()
-        subscribe(args_url, args_api_key, signals, on_subscribe=self.redirect_vhal_props_to_device, on_change=False)
+        subscribe(args_url, args_api_key, signals, on_subscribe=self.redirect_vhal_props_to_device, on_change=True)
 
 
 if __name__ == "__main__":
@@ -38,3 +49,7 @@ if __name__ == "__main__":
     adb_device = adb.get_emulator_device()
     br_dev = brokerDeviceBridge(adb_device)
     br_dev.set_vhal_props()
+
+    for name, val in getmembers(vhal_props):
+        if val == 291504647:
+            print(f"Property name: {name}")
--- libs/vhal_emulator/vhal_emulator.py
+++ libs/vhal_emulator/vhal_emulator.py
@@ -107,6 +107,8 @@ class Vhal:
         # Serialize the protobuf into a string
         msgStr = cmd.SerializeToString()
         msgLen = len(msgStr)
+        # Debug print: print the protobuf message as hex
+        print("Serialized protobuf bytes:", msgStr)
         # Convert the message length into int32 byte array
         msgHdr = struct.pack('!I', msgLen)
         # Send the message length first
@@ -217,17 +219,17 @@ class Vhal:
             raise ValueError('propId is invalid:', prop)
 
         propValue.value_type = valType
-        if valType in self._types.TYPE_STRING:
+        if valType == self._types.TYPE_STRING:
             propValue.string_value = value
-        elif valType in self._types.TYPE_BYTES:
+        elif valType == self._types.TYPE_BYTES:
             propValue.bytes_value = value
-        elif valType in self._types.TYPE_INT32:
+        elif valType == self._types.TYPE_INT32:
             propValue.int32_values.append(value)
-        elif valType in self._types.TYPE_INT64:
+        elif valType == self._types.TYPE_INT64:
             propValue.int64_values.append(value)
-        elif valType in self._types.TYPE_FLOAT:
+        elif valType == self._types.TYPE_FLOAT:
             propValue.float_values.append(value)
-        elif valType in self._types.TYPE_MIXED:
+        elif valType == self._types.TYPE_MIXED:
             propValue.string_value = \
                 get_by_attribute_or_key(value, 'string_value', '')
             propValue.bytes_value = \
@@ -255,3 +257,4 @@ class Vhal:
         # Parse the list of configs to generate a dictionary of prop_id to type
         for cfg in msg.config:
             self._propToType[cfg.prop] = cfg.value_type
+
