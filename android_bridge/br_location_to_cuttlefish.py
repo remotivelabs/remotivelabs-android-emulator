c2ee90be39cd22ba50ecaf18979da8588f5d25cc
Jacob Ferm <jacob.ferm@remotivelabs.com>
2025-10-15 13:27:49 +0200

    refactor: refactor and clean up the cuttlefish code

--- android_bridge/br_location_to_cuttlefish.py
+++ android_bridge/br_location_to_cuttlefish.py
@@ -1,48 +1,51 @@
+from remotivelabs.broker import FrameName, SignalValue
 import requests
 import json
 
 
-class BrokerToRest:
-    def __init__(self, rest_url, signals):
-        self.rest_url = rest_url
-        self.signal_name_latitude = signals[0][1]
-        self.signal_name_longitude = signals[1][1]
+class BrokerToCuttlefish:
+    def __init__(
+        self,
+        cuttlefish_url,
+        longitude_signal_name,
+        latitude_signal_name,
+    ):
+        self.cuttlefish_url = cuttlefish_url
+        self.longitude_signal_name = longitude_signal_name
+        self.latitude_signal_name = latitude_signal_name
         self.lat = None
         self.lon = None
 
-    def send_location_via_rest(self, latitude, longitude, elevation=15.0):
-        payload = {
-            "delay": 0,
-            "coordinates": [
-                {"latitude": latitude, "longitude": longitude, "elevation": elevation}
-            ],
-        }
-        headers = {"Content-Type": "application/json"}
+    def redirect_location_signals_to_cuttlefish(
+        self, name: FrameName, value: SignalValue
+    ):
+        if name == self.longitude_signal_name:
+            self.lon = float(value)
+        if name == self.latitude_signal_name:
+            self.lat = float(value)
 
-        try:
-            response = requests.post(
-                self.rest_url + "/services/GnssGrpcProxy/SendGpsVector",
-                headers=headers,
-                data=json.dumps(payload),
-                verify=False,  # Only use this in dev/testing
-                timeout=10,  # Set a timeout of 10 seconds
-            )
-            response.raise_for_status()
-            print("Location sent successfully.")
-        except requests.RequestException as e:
-            print(f"Failed to send location: {e}")
+        if self.lat is not None and self.lon is not None:
+            payload = {
+                "delay": 0,
+                "coordinates": [
+                    {
+                        "latitude": self.lat,
+                        "longitude": self.lon,
+                        "elevation": 15,
+                    }
+                ],
+            }
+            headers = {"Content-Type": "application/json"}
 
-    def redirect_location_to_rest(self, signals):
-        for signal in signals:
-            print(
-                "{} {} {} ".format(
-                    signal.id.name, signal.id.namespace.name, signal.double
+            try:
+                response = requests.post(
+                    self.cuttlefish_url + "/services/GnssGrpcProxy/SendGpsVector",
+                    headers=headers,
+                    data=json.dumps(payload),
+                    verify=False,  # Only use this in dev/testing
+                    timeout=10,  # Set a timeout of 10 seconds
                 )
-            )
-            if signal.id.name == self.signal_name_latitude:
-                self.lat = signal.double
-            elif signal.id.name == self.signal_name_longitude:
-                self.lon = signal.double
-
-            if self.lat is not None and self.lon is not None:
-                self.send_location_via_rest(self.lat, self.lon)
+                response.raise_for_status()
+                print("Location sent successfully.")
+            except requests.RequestException as e:
+                print(f"Failed to send location: {e}")
