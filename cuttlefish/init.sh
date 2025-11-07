80e51701670460c0d7af658ffa46255a3408af04
Jacob Ferm <jacob.ferm@remotivelabs.com>
2025-11-07 14:20:37 +0100

    fix: improve cuttlefish startup behaviour
    On some machines it sometimes failt to boot or got stuck due to
    attempting to start cuttlefish before the host tools were started.

--- cuttlefish/init.sh
+++ cuttlefish/init.sh
@@ -1,34 +1,42 @@
 #!/usr/bin/env bash
 
-ulimit -n 4096 # Increase open files limit for launch_cvd
+# Increase open files limit for launch_cvd
+ulimit -n 4096
 
-./run_services.sh & # Existing orchestrator startup script
+# Existing orchestrator startup script
+./run_services.sh &
 
+# Wait for orchestrator to start
+until nc -z localhost 2081 2>/dev/null; do sleep 1; done
 sleep 5
 
+# Launch Cuttlefish image
 ./bin/adb devices
 ./bin/launch_cvd --daemon \
-  --resume=false \
   --guest_enforce_security=false \
   --enable_vhal_proxy_server \
   --display=width=1400,height=800,dpi=160,refresh_rate_hz=30 \
   --display=width=600,height=800,dpi=160,refresh_rate_hz=30 \
   --report_anonymous_usage_stats=n
-sleep 2
+sleep 3
 
 ./bin/adb connect localhost:6520
 ./bin/adb wait-for-device
 ./bin/adb root
 
+# Install optional APKs
 for apk in apks/*.apk; do
   ./bin/adb install "$apk"
 done
 
+# Enable wifi and connect to network
 ./bin/adb shell svc wifi enable
 ./bin/adb shell cmd wifi connect-network VirtWifi open
-./bin/adb shell cmd car_service enable-uxr false # Allow apps to be used while driving
+# Allow apps to be used while driving
+./bin/adb shell cmd car_service enable-uxr false
 
-socat TCP-LISTEN:9300,fork,reuseaddr,bind=$(hostname -i) TCP:192.168.98.1:9300 & # Redirect from container port to vhal_proxy_server port
+# Reroute container port 9300 to internal VHAL proxy server
+socat TCP-LISTEN:9300,fork,reuseaddr,bind=$(hostname -i) TCP:192.168.98.1:9300 &
 
 echo "Cuttlefish is started and ready to use"
 
